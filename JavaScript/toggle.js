7ed0e0e292110a1af091d3876dd50ddb5802224f
GregV3 <grzegorz.muszynski77@gmail.com>
2022-12-20 14:20:51 +0100

    Removing comments

--- JavaScript/toggle.js
+++ JavaScript/toggle.js
@@ -1,6 +1,6 @@
-{/* <script type="text/javascript"> */}
-$(".menu-toggle-btn").click(function(){
+  // After each time, the function below deletes or adds classes according if there are or are not. If yes - delete, if not - add
+$(".menu-toggle-btn").click(function() {
+    // fa-times - cross imported from font-awesome cross
   $(this).toggleClass("fa-times");
   $(".navigation-menu").toggleClass("active");
 });
-// </script>
